c00a482214e8bab8d6a9b50eb1d9c4f43a08b773
Makar_Jv <makar_igor1997@mail.ru>
2022-01-13 18:49:10 +0300

    Init push

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <component :is="layout">
+      <router-view/>
+    </component>
+  </div>
+</template>
+
+<script>
+import EmptyLayout from './layouts/EmptyLayout'
+import MainLayout from './layouts/MainLayout'
+export default {
+  components: {EmptyLayout},
+  computed:{
+    layout() {
+      console.log(this.$route.meta)
+      return 'main-layout'
+    }
+  },
+  comments: {
+    EmptyLayout, MainLayout
+  }
+}
+</script>
+<style lang="scss">
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
+@import url('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js');
+@import url('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css');
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+</style>
