3cc00bd0b8c6820b3b281414cf53aac4794350c1
makar <makar_igor1997@mail.ru>
2022-01-15 01:08:15 +0300

    Config Login.vue

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import router from './router.js'
 import store from './store.js'
 import 'materialize-css/dist/js/materialize.min'
 import dateFilter from '@/filters/date.filter.js'
+import Vuelidate from 'vuelidate'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
 new Vue({
