d5f6f406b650804b26d7097474f524705405a4eb
makar <makar_igor1997@mail.ru>
2022-01-17 01:20:00 +0300

    x

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,6 +3,8 @@ export default {
 	actions: {
 		async login({email, password}) {
 			try {
+				console.log(email)
+				console.log(password)
 				await firebase.auth().signInWithEmailAndPassword(email, password)
 			} catch (e) {
 				console.log(e)
