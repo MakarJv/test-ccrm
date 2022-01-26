6664ea214bc64af66745aa5f67108a371e3718ac
makar <makar_igor1997@mail.ru>
2022-01-26 15:12:36 +0300

    Add paginations

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import currencyFilter from './filters/currency.filter'
 import messagePlugin from './utils/message.plagin'
 import Loader from "./components/app/Loader"
 import tooltipDirective from './directives/tooltip.directive'
+import Paginate from 'vuejs-paginate'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
@@ -23,6 +24,7 @@ Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
 
 firebase.initializeApp({
 	apiKey: "AIzaSyA2T8VkVqu9FU8XMrkx92Vj0GwCSuDOnwk",
